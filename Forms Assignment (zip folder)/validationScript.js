5e489ac5a4f2e64641229598e5efb3606f1729f4
Rahul Murgai <ramurgai@my.waketech.edu>
2025-03-28 12:32:03 -0400

    learning form validation

---
+++ Forms Assignment (zip folder)/validationScript.js
@@ -0,0 +1,29 @@
+    // JavaScript code for form validation
+	// Prevent form from submitting
+
+      // Retrieve the input field value
+
+      // Regular expression pattern for alphanumeric input
+
+      // Check if the input value matches the pattern
+
+        // Valid input: display confirmation and submit the form
+
+        // Invalid input: display error message
+
+
+let submit = document.getElementById("submit")
+submit.addEventListener("click", validEntry);
+
+function validEntry() {
+  let inp = document.getElementById("inputField")
+  if (inp.validity.valueMissing){
+    inp.setCustomValidity("Please input an alphanumeric value")
+  }
+  else if ((/[a-z]$/.test(inp.value)) || (/[A-Z]$/.test(inp.value)) || (/[0-9]$/.test(inp.value))){
+    inp.setCustomValidity("Input correct.")
+  }
+  else {
+    inp.setCustomValidity("Your input is not alphanumeric.")
+  }
+}
