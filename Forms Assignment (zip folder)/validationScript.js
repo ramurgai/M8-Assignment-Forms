c309d28b846fb25c702109bd4d7cbf5541c94e06
Rahul Murgai <ramurgai@my.waketech.edu>
2025-03-28 12:52:34 -0400

    learning form validation

--- Forms Assignment (zip folder)/validationScript.js
+++ Forms Assignment (zip folder)/validationScript.js
@@ -17,7 +17,7 @@ submit.addEventListener("click", validEntry);
 
 function validEntry() {
   let inp = document.getElementById("inputField")
-  if (inp.validity.valueMissing){
+  if (inp.validity.valueMissing) {
     inp.setCustomValidity("Please input a value")
   }
   else if ((/[a-z]$/.test(inp.value)) || (/[A-Z]$/.test(inp.value)) || (/[0-9]$/.test(inp.value))){
